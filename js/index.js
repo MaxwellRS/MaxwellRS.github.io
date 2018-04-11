ca731459fc8c03d6c2493f9234401946685e250f
Max Strusowski <36252202+MaxwellRS@users.noreply.github.com>
2018-04-11 12:57:51 +0900

    resize for mobile

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   
+  $(window).on('load resize',function(){
+        if($(window).width() < 950){
+            window.location = "https://codepen.io/okmaxlegroom/full/EEGNBR/"
+        }
+    });
+  
   $('ul').on('click', 'li a', function() {
     $('ul li a.active').removeClass('active');
     $(this).addClass('active');
